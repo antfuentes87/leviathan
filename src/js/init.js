fcf864cc67a9cc007bc0eea49edd0e4a8cad0182
Anthony Fuentes <antfuentes87@gmail.com>
2015-08-25 14:26:34 -0400

    revamp, new direction

--- src/js/init.js
+++ src/js/init.js
@@ -1,36 +1,10 @@
-/*var callback = function() {
-		var elements = document.querySelectorAll('.section');
-		for (var i = 0; i < elements.length; i++) {
-				var rowHeight = $(elements[i]).outerHeight();
-				$(elements[i]).height(rowHeight);
-		}
-};*/
-
-window.viewportUnitsBuggyfill.init({
-	refreshDebounceWait: 5,
-	hacks: window.viewportUnitsBuggyfillHacks
-});
-
-$('.col').matchHeight({
-	byRow: true,
-	property: 'height',
-	target: null,
-	remove: false
-});
-
-$('h2').matchHeight({
-	byRow: true,
-	property: 'height',
-	target: null,
-	remove: false
-});
-
-$('p').matchHeight({
-	byRow: true,
-	property: 'height',
-	target: null,
-	remove: false
-});
-
-$(document).ready(callback);
-$(window).load(callback);
+$.fn.matchHeight._beforeUpdate = function(event, groups) {
+		$("[data-min-height]").each(function() {
+				var dataHeight = $(this).data("min-height");
+				var dataPercent = dataHeight / 100;
+				$(this).css("min-height", function() {
+						return $(window).height() * dataPercent;
+				});
+		});
+		$('.vertical').flexVerticalCenter();
+};
