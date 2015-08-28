28c83d693cb16c2256b2e436ea76485496e1dbf2
antfuentes87 <antfuentes87@gmail.com>
2015-08-27 22:37:02 -0500

    created dist and init bower

--- src/js/init.js
+++ src/js/init.js
@@ -1,10 +1,10 @@
 $.fn.matchHeight._beforeUpdate = function(event, groups) {
-		$("[data-min-height]").each(function() {
-				var dataHeight = $(this).data("min-height");
-				var dataPercent = dataHeight / 100;
-				$(this).css("min-height", function() {
-						return $(window).height() * dataPercent;
-				});
+	$("[data-min-height]").each(function() {
+		var dataHeight = $(this).data("min-height");
+		var dataPercent = dataHeight / 100;
+		$(this).css("min-height", function() {
+			return $(window).height() * dataPercent;
 		});
-		$('.vertical').flexVerticalCenter();
+	});
+	$('.vertical').flexVerticalCenter();
 };
