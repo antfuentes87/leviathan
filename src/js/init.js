283911ceb39f7cb49c588691aae1c5c9687292fa
antfuentes87 <antfuentes87@gmail.com>
2015-08-28 17:59:59 -0500

    created pure javascript min-height and vertical center // changed dependants to jquery - matchHeight - gsap

--- src/js/init.js
+++ src/js/init.js
@@ -1,10 +1,19 @@
-$.fn.matchHeight._beforeUpdate = function(event, groups) {
-	$("[data-min-height]").each(function() {
-		var dataHeight = $(this).data("min-height");
+function init(){
+ 	var arr = document.querySelectorAll("[data-min-height]");
+	for (var i = 0; i < arr.length; i++){
+		var currentWindowHeight = window.innerHeight;
+		var dataHeight = arr[i].dataset.minHeight;
 		var dataPercent = dataHeight / 100;
-		$(this).css("min-height", function() {
-			return $(window).height() * dataPercent;
-		});
-	});
-	$('.vertical').flexVerticalCenter();
-};
+		var dataPercentHeight = currentWindowHeight * dataPercent;
+		arr[i].style.minHeight = dataPercentHeight + "px";
+	}
+	
+	var newArr = document.querySelectorAll(".vertical");	
+	for (var z = 0; z < arr.length; z++){
+		var verticalHeight = newArr[z].offsetHeight;
+		var parentHeight = newArr[z].parentNode.offsetHeight;
+		var marginTop = (parentHeight - verticalHeight) / 2;
+		newArr[z].style.marginTop = marginTop + "px";
+	}
+	window.onresize = init;
+}
