be85f9417b7a04e824a81ec6cfe720be7e7b8a86
decayedcross <decayedcross@gmail.com>
2015-09-17 19:47:30 -0700

    fb, g+, youtube

--- src/js/gplus.js
+++ src/js/gplus.js
@@ -1,2 +1,8 @@
-
-src="https://apis.google.com/js/platform.js" async defer
+<g:plus action="share" annotation="none"></g:plus>
+<script type="text/javascript">
+	(function() {
+		var po = document.createElement('script'); po.type = 'text/javascript'; po.async = true;
+		po.src = 'https://apis.google.com/js/plusone.js';
+		var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(po, s);
+	})();
+</script>
